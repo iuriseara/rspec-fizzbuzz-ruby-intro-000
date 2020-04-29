96f66caba42a196fd16e26947da52a0c78d89cdf
fearless-command-2358 <iuriseara@gmail.com>
2020-04-29 18:11:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './fizzbuzz_spec.rb'
 
-fizzbuzz = gets.chomp
-
-def fizzbuzz()
-if fizz_3 = fizzbuzz(3)
-  expect(fizz_3).to eq("Fizz")
-
-#elsif fizzbuzz / 5
-#  puts "Buzz"
-
-#elsif fizzbuzz / 15
-#  puts "Fizzbuzz"
-else
+def fizzbuzz(int)
+  if (int % 3 == 0) && (int % 5 == 0)# if the number int is divisible by 3
+    "FizzBuzz" # Go fizz
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  else
+    nil
+  end
+end
