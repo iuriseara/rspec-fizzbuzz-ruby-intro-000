63089fd7b7807f620f2769144057b74a869e42c2
fearless-command-2358 <iuriseara@gmail.com>
2020-04-29 18:43:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require './fizzbuzz.rb'
+require_relative './fizzbuzz.spec.rb'
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
-  end
+  if fizz_3 = fizzbuzz(3)
+    print "Fizz"
+  elsif fizz_5 = fizzbuzz(5)
+    print "Buzz"
+  elsif fizz_15 = fizzbuzz(15)
+    print "Fizzbuzz"
+  else fizz_4 = fizzbuzz(4)
+    print nil
 end
