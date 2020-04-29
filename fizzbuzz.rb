49e65189c998794f4c016a0cd6f62a652e0726b4
fearless-command-2358 <iuriseara@gmail.com>
2020-04-29 18:25:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-class FizzBuzz
-  def self.convert(input_numbers)
-    if (input_numbers%15) == 0
-      'FizzBuzz'
-    elsif (input_numbers%5) == 0
-      'Buzz'
-    elsif (input_numbers%3) == 0
-      'Fizz'
-    else
-      input_numbers
-    end
-  end
+def fizzbuzz
+
 end
