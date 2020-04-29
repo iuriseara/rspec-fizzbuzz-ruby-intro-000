b981e245fcbdfec6e0395014178275e2ea04e5a3
fearless-command-2358 <iuriseara@gmail.com>
2020-04-29 18:04:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './fizzbuzz.rb'
+require_relative './fizzbuzz_spec.rb'
+
+fizzbuzz = gets.chomp
 
 def fizzbuzz()
-  if / 3
-    puts "Fizz"
-  elsif
+if fizz_3 = fizzbuzz(3)
+  expect(fizz_3).to eq("Fizz")
+
+#elsif fizzbuzz / 5
+#  puts "Buzz"
+
+#elsif fizzbuzz / 15
+#  puts "Fizzbuzz"
+else
